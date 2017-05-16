d471c4f909dc51061cbfc1c998539d7f2a4ed3f5
Jasmeet Singh Kohli <jasmeetkohlisingh@gmail.com>
2017-05-16 11:52:53 +0530

    Update write_from_file.py

--- my_vault/actions/write_from_file.py
+++ my_vault/actions/write_from_file.py
@@ -6,10 +6,10 @@ from lib import action
 class VaultWriteAction(action.VaultBaseAction):
     def run(self, path, secret_file):
         try:
-			with open(secret_file,"r+") as myfile:
-				secrets = myfile.read()
+		with open(secret_file,"r+") as myfile:
+			secrets = myfile.read()
 
-			return self.vault.write(path, **yaml.load(secrets))
-		except Exception as e:
-			print ("Exception in write to vault from file : " , e)
-			exit(1)
+		return self.vault.write(path, **yaml.load(secrets))
+	except Exception as e:
+		print ("Exception in write to vault from file : " , e)
+		exit(1)
